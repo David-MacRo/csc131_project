fc8d41085da4c6eed0689915cf4a3a221b3028b9
Kher-ops <official.king21@gmail.com>
2026-03-08 13:58:41 -0700

    New change! I don't know if this is how you wanted it.

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,5 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <fstream>
-#include <sstream>
-#include <filesystem>
-#include <regex>
-#include <cstdlib>
+#include "pipeline.hpp"
 
-using namespace std;
-
-struct Record {
-    string name;
-    string source;
-    string dob;
-    string address;
-
-    vector<string> companies;
-
-    Record() = default;
-};
-
-struct Node {
-    Record data;
-    Node* prev = nullptr;
-    Node* next = nullptr;
-    explicit Node(const Record& r) : data(r) {}
-};
-
-Class DoublyLinkedList {
-private:
-    Node* head = nullptr;
-    Node* tail = nullptr;
-    size_t sz = 0;
-
-public: 
-    ~DoublyLinkedList () {clear(); }
-
-    void push_back(const Record& r) {
-        Node* n = new Node(r);
-        if (!tail) {
-            head = tail = n;
-        } else {
-            tail->next = n;
-            n->prev = tail;
-            tail = n;
-        }
-        ++sz;
-    }
+int main(int argc, char** argv) {
+    return run_pipeline(argc, argv);
 }
