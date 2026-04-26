bb203b9d5bda8186158b336977c8109cb10c062a
David-Macro <david.macroberts@outlook.com>
2026-04-26 12:36:42 -0700

    fstring slashes for config directory pathing

--- src/config.py
+++ src/config.py
@@ -1,18 +1,18 @@
 import json
 import os
 
-with open("src/config.json", 'r') as file:
-    variables = json.load(file)
-
 SLASH = ""
 if(os.name == "NT"): #windows
     SLASH = "\\"
 else:                #posix
     SLASH = "/"
 
+with open(f"src{SLASH}config.json", 'r') as file:
+    variables = json.load(file)
+
 BASE_URL = variables.get("base_url")
 SOL_START = variables.get("sol_start")
 SOL_END = variables.get("sol_end")
-SUMMARY_PATH = "." + SLASH + variables.get("summary_path")
-DATA_PATH = variables.get("data_path")
+SUMMARY_PATH = f".{SLASH}{variables.get("summary_path")}"
+DATA_PATH = f".{SLASH}{variables.get("data_path")}"
 
