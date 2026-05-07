4598adf0c7f77b7b9fd24b27886f83cbd03d0eac
David-Macro <david.macroberts@outlook.com>
2026-05-07 15:58:59 -0700

    remove old parser, because we aren't using pdfs now

--- src/voting_data_scraper.py
+++ src/voting_data_scraper.py
@@ -1,7 +1,6 @@
 from persons import save_all_persons_votes, get_persons_from_file, find_all_event_items_voters, find_matter_votes
 from agendas import save_event_item_ids, get_event_item_ids_from_file, fetch_all_summaries, save_event_item_votes
-from parse_summaries import search_summaries_for_match
-    
+
 def main() -> None:
     # gets all persons who have voted and their votes and saves it to a file
     # can be commented out after it has been ran once for testing
@@ -23,9 +22,6 @@ def main() -> None:
     # downloads the "Summary Report" for the agendas that have been voted on
     # can be commented out after it has been ran once for testing
     save_event_item_votes(event_item_ids)
-
-    # parses through all summaries for a list of strings
-    matches = search_summaries_for_match(["Landpaths"])
 
     # parses the previous event item voters list to see who voted and how they voted on the matched matter
     # find_matter_votes(voted_items_ids, matches)
