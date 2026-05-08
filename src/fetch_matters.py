53e55889219936d15d3acb45c81178dcdcafd427
David-Macro <david.macroberts@outlook.com>
2026-05-07 17:02:45 -0700

    commented in plans

---
+++ src/fetch_matters.py
@@ -0,0 +1,16 @@
+def fetch_list(matter_links: list, folder: str):
+    #open checked from folder
+    
+    #remove already checked items from list
+
+    #create threads and split into sublists
+
+    #await threads
+
+    #merge returned filename sublists
+
+    #write out filename list to folder
+
+    #append newly checked urls to folder
+
+    #return list of filenames
